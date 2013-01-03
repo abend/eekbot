9c3ed663dbdb3643605b40aa18132d30608085ad
Sasha Kovar <sasha-git@arcocene.org>
2013-01-02 16:11:43 -0800

    Floorbot: fix math; query max motor speeds.

--- nxt/Floorbot.java
+++ nxt/Floorbot.java
@@ -6,18 +6,17 @@ import java.util.*;
  * Floorbot controls a robot with two threaded rods and a remote stylus.
  */
 public class Floorbot {
-        static final int MAX_SPEED = 300;
-        //static final int PEN_SPEED = 50;
-        //static final int PEN_ROT = 30;
-        static final int OFF_X = 48;
-        static final int OFF_Y = 0;
-        static final int WIDTH = 610; // mm
-        static final double MM_REV = 1;//4.762; // mm per revolution
-        static final int START_X = 0;//WIDTH / 2;
+        /** Span between the center of the base points of the triangle. */
+        static final int WIDTH = 450; // mm
+        static final int OFF_X = WIDTH / 2;
+        /** Distance from center of base to drawing tip when tip is at logical 0,0. */
+        static final int OFF_Y = 540;
+        static final double MM_REV = 1; // mm per revolution
+        static final int START_X = 0;
         static final int START_Y = START_X;
         static int curX = START_X;
         static int curY = START_Y;
-        static boolean penDown = true;
+        static final int scale = 2;
 
 
         public static void main(String[] args) {
@@ -25,16 +24,16 @@ public class Floorbot {
                 //RConsole.openUSB(15000);
                 //RConsole.openBluetooth(1000);
 
-                //int[] pos = Data.data;
-                int[] pos = PresetShapes.square();
+                int[] pos = Data.data;
+                //int[] pos = PresetShapes.star();
 
                 int total = pos.length / 3;
 
                 for (int i = 0; i < pos.length;) {
                         //int pen = pos[i];
-                        int x = pos[i+1];
-                        int y = pos[i+2];
-                        //if (pen == 0) penUp(); else penDown();
+                        int x = pos[i+1] * scale;
+                        int y = pos[i+2] * scale;
+
                         int step = (int)(i / 3);
                         RConsole.println(step + "/" + total + ": going to " + x + "," + y);
                         LCD.drawString(step + "/" + total, 0, 3);
@@ -45,9 +44,9 @@ public class Floorbot {
                 //Button.waitForAnyPress(15000);
                 //RConsole.close();
                 //LCD.drawString("All Done!", 0, 3);
-                Sound.playTone(500, 150);
-                Sound.playTone(1000, 150);
-                Sound.playTone(2000, 5000);
+                Sound.playTone(1000, 250);
+                Sound.playTone(2000, 250);
+                Sound.playTone(4000, 500);
         }
 
         static boolean go(int px, int py) {
@@ -64,23 +63,27 @@ public class Floorbot {
                 int db = mmToDegrees(diffb);
 
                 // the larger goes at max speed
-                int spdA = 0;
-                int spdB = 0;
+                float spdA = 0;
+                float spdB = 0;
+
+                float maxSpdA = Motor.A.getMaxSpeed();
+                float maxSpdB = Motor.B.getMaxSpeed();
+                float maxSpd = Math.min(maxSpdA, maxSpdB);
 
                 if (Math.abs(da) > 0 && Math.abs(db) > 0) {
                         if (Math.abs(da) > Math.abs(db)) {
-                                spdA = MAX_SPEED;
-                                spdB = (int)Math.abs(((float)db / (float)da) * (float)MAX_SPEED);
+                                spdA = maxSpd;
+                                spdB = (int)Math.abs(((float)db / (float)da) * (float)maxSpd);
                         } else {
-                                spdA = (int)Math.abs(((float)da / (float)db) * (float)MAX_SPEED);
-                                spdB = MAX_SPEED;
+                                spdA = (int)Math.abs(((float)da / (float)db) * (float)maxSpd);
+                                spdB = maxSpd;
                         }
                 } else if (Math.abs(da) > 0) {
-                        spdA = MAX_SPEED;
+                        spdA = maxSpd;
                         spdB = 0;
                 } else if (Math.abs(db) > 0) {
                         spdA = 0;
-                        spdB = MAX_SPEED;
+                        spdB = maxSpd;
                 // } else {
                 //         RConsole.println("speed confusion " + da + "," + db);
                 //         return false;
@@ -91,20 +94,23 @@ public class Floorbot {
                         //return;
                 }
 
-                RConsole.println("  dist: " + da + "," + db);
-                RConsole.println("  speed: " + spdA + "," + spdB);
+                //RConsole.println("  dist: " + da + "," + db);
+                //RConsole.println("  speed: " + spdA + "," + spdB);
+                LCD.drawString("dst: " + diffa + "," + diffb, 0, 4);
+                LCD.drawString("rot: " + da + "," + db, 0, 5);
+                LCD.drawString("spd: " + spdA + "," + spdB, 0, 6);
 
                 if (spdA > 0) Motor.A.setSpeed(spdA);
                 if (spdB > 0) Motor.B.setSpeed(spdB);
 
                 if (spdA > 0) Motor.A.rotate(da, true);
-                if (spdB > 0) Motor.B.rotate(db, true);
+                if (spdB > 0) Motor.B.rotate(-db, true); // reverse spin, motor faces other direction
 
                 boolean keep = true;
                 while (keep &&
-                       (Motor.A.isMoving() || Motor.B.isMoving())) 
+                       (Motor.A.isMoving() || Motor.B.isMoving()))
                 {
-                        if (Button.ENTER.isDown() || 
+                        if (Button.ENTER.isDown() ||
                             Button.ENTER.isDown()) {
                                 keep = false;
                         }
@@ -119,11 +125,15 @@ public class Floorbot {
                 return keep;
         }
 
+        /**
+         * Convert from a rectangular coordinate to lengths of the
+         * rods.
+         */
         static int[] rect2hyp(double px, double py) {
-                double a = Math.sqrt(Math.pow(px - OFF_X, 2) +
-                                     Math.pow(py - OFF_Y, 2));
-                double b = Math.sqrt(Math.pow(WIDTH - px - OFF_X, 2) +
-                                     Math.pow(py - OFF_Y, 2));
+                double a = Math.sqrt(Math.pow(OFF_X + px, 2) +
+                                     Math.pow(OFF_Y + py, 2));
+                double b = Math.sqrt(Math.pow(OFF_X - px, 2) +
+                                     Math.pow(OFF_Y + py, 2));
                 int[] p = {(int)a,(int)b};
                 return p;
         }
@@ -131,18 +141,4 @@ public class Floorbot {
         static int mmToDegrees(int mm) {
                 return (int)((mm / MM_REV) * 360);
         }
-
-        // static void penUp() {
-        //         if (!penDown) return;
-        //         Motor.C.setSpeed(PEN_SPEED);
-        //         Motor.C.rotate(-PEN_ROT);
-        //         penDown = false;
-        // }
-
-        // static void penDown() {
-        //         if (penDown) return;
-        //         Motor.C.setSpeed(PEN_SPEED);
-        //         Motor.C.rotate(PEN_ROT);
-        //         penDown = true;
-        // }
 }
